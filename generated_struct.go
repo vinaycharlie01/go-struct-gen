9166c6c0e65ed0a1f0cd8a55d85e921281599d35
VinayKumar V A <106230026+vinaycharlie01@users.noreply.github.com>
2023-12-25 08:11:45 +0000

    Addded more accureate

--- generated_struct.go
+++ generated_struct.go
@@ -1,52 +1,27 @@
 package main
 
-type EventData struct {
-	Name string `json:"name,omitempty"`
-	Status string `json:"status,omitempty"`
-	Key string `json:"Key,omitempty"`
-	Event Event `json:"Event,omitempty"`
+type EventData1 struct {
+	ID int `json:"id,omitempty"`
+	Owner Owner `json:"owner,omitempty"`
 }
-type Comp struct {
-			CompType string `json:"CompType,omitempty"`
-			CompTypeInt int `json:"CompTypeInt,omitempty"`
-			CompId int `json:"CompId,omitempty"`
-		}
-type PrimaryComp struct {
-		CompType string `json:"CompType,omitempty"`
-		CompTypeInt int `json:"CompTypeInt,omitempty"`
-		CompId int `json:"CompId,omitempty"`
-		Comp Comp `json:"Comp,omitempty"`
-		}
-type Tier struct {
-				TierType string `json:"TierType,omitempty"`
-				TierInt int `json:"TierInt,omitempty"`
-			}
-type MaintModeReason struct {
-					MMReason string `json:"MMReason,omitempty"`
-					MMInt int `json:"MMInt,omitempty"`
-					MMLongRun string `json:"MMLongRun,omitempty"`
-				}
-type Event struct {
-	Node int `json:"Node,omitempty"`
-	Seq int `json:"Seq,omitempty"`
-	Eincn int `json:"Eincn,omitempty"`
-	Sev int `json:"Sev,omitempty"`
-	Type string `json:"Type,omitempty"`
-	Time int `json:"Time,omitempty"`
-	HiResTime int64 `json:"HiResTime,omitempty"`
-	MessageCode int `json:"MessageCode,omitempty"`
-	AlertId int `json:"AlertId,omitempty"`
-	Class string `json:"Class,omitempty"`
-	PrimaryComp PrimaryComp `json:"PrimaryComp,omitempty"`
-			ComponentKey string `json:"ComponentKey,omitempty"`
-			Tier Tier `json:"Tier,omitempty"`
-				AlertTypeInt int `json:"AlertTypeInt,omitempty"`
-				MaintModeReason MaintModeReason `json:"MaintModeReason,omitempty"`
-					MsgParams string `json:"MsgParams,omitempty"`
-					NonscAlertMetaInt int `json:"NonscAlertMetaInt,omitempty"`
-					ShortDesc string `json:"ShortDesc,omitempty"`
-					EventString string `json:"EventString,omitempty"`
-					Serial string `json:"Serial,omitempty"`
-					SystemModel string `json:"SystemModel,omitempty"`
-					ReleaseLevel string `json:"ReleaseLevel,omitempty"`
-				}
+type Owner struct {
+	Login string `json:"login,omitempty"`
+	ID int `json:"id,omitempty"`
+	AvatarURL string `json:"avatar_url,omitempty"`
+	GravatarID string `json:"gravatar_id,omitempty"`
+	URL string `json:"url,omitempty"`
+	HTMLURL string `json:"html_url,omitempty"`
+	FollowersURL string `json:"followers_url,omitempty"`
+	FollowingURL string `json:"following_url,omitempty"`
+	GistsURL string `json:"gists_url,omitempty"`
+	StarredURL string `json:"starred_url,omitempty"`
+	SubscriptionsURL string `json:"subscriptions_url,omitempty"`
+	OrganizationsURL string `json:"organizations_url,omitempty"`
+	ReposURL string `json:"repos_url,omitempty"`
+	EventsURL string `json:"events_url,omitempty"`
+	ReceivedEventsURL string `json:"received_events_url,omitempty"`
+	Type string `json:"type,omitempty"`
+	SiteAdmin bool `json:"site_admin,omitempty"`
+	Listdata []int `json:"listdata,omitempty"`
+	Hello []any `json:"hello,omitempty"`
+}
